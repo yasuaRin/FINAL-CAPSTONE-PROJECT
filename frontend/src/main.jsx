f72b5cfabcef15d3515df0076f07202056f433ee
Hera <shynnyshaquille@gmail.com>
2026-05-09 11:20:30 +0700

    feat: add leads feature

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,12 @@
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles/index.css';
+import { PartnerProvider } from './context/PartnerContext';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <PartnerProvider>
+      <App />
+    </PartnerProvider>
   </React.StrictMode>
 );
