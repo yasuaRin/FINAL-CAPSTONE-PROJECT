13cb0da819259cc9bc26fc9a278f3d9f16fda331
Hera <shynnyshaquille@gmail.com>
2026-05-10 23:24:42 +0700

    fix: password reset session handling

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,10 +4,19 @@ import App from './App';
 import './styles/index.css';
 import { PartnerProvider } from './context/PartnerContext';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <PartnerProvider>
-      <App />
-    </PartnerProvider>
-  </React.StrictMode>
-);
+const href = window.location.href;
+const codeMatch = href.match(/[?&]code=([^&#]+)/);
+if (codeMatch) {
+  sessionStorage.setItem('reset_code', codeMatch[1]);
+  window.location.replace(
+    window.location.origin + '/#/admin/auth/reset-password'
+  );
+} else {
+  ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+      <PartnerProvider>
+        <App />
+      </PartnerProvider>
+    </React.StrictMode>
+  );
+}
